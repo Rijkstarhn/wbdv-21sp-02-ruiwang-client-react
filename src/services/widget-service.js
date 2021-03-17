4cfaa6295af4d678281ac32f78e100f7ca5f6c81
Rijkstarhn <32129291+Rijkstarhn@users.noreply.github.com>
2021-03-17 15:03:17 -0700

    Created Widget service.

---
+++ src/services/widget-service.js
@@ -0,0 +1,38 @@
+const WIDGET_URL = 'http://localhost:8080/api'
+
+export const createWidget = (tid, widget) =>
+    fetch(`${WIDGET_URL}/topics/${tid}/widgets`, {
+        method: 'POST',
+        body: JSON.stringify(widget),
+        headers: {
+            'content-type':'application/json'
+        }
+    }).then(res => res.json())
+
+export const findWidgetsForTopic = tid =>
+    fetch(`${WIDGET_URL}/topics/${tid}/widgets`).then(
+        res => res.json()
+    )
+
+export const updateWidget = (wid, widget) =>
+    fetch(`${WIDGET_URL}/widgets/${wid}`, {
+        method: 'PUT',
+        body: JSON.stringify(widget),
+        headers: {
+            'content-type':'application/json'
+        }
+    }).then(res => res.json())
+
+export const deleteWidget = wid =>
+    fetch(`${WIDGET_URL}/widgets/${wid}`, {
+        method: 'DELETE',
+    }).then(res => res.json())
+
+const api = {
+    createWidget,
+    findWidgetsForTopic,
+    updateWidget,
+    deleteWidget,
+}
+
+export default api
