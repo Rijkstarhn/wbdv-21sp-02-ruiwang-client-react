5db8294ab68f42236e9185c4c584aa0a392e419c
Rijkstarhn <32129291+Rijkstarhn@users.noreply.github.com>
2021-03-17 23:47:11 -0700

    Finished heading and paragraph widget.

--- src/components/course-editor/widgets/heading-widget.js
+++ src/components/course-editor/widgets/heading-widget.js
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const HeadingWidget = () => {
-
+const HeadingWidget = ({widget, updateWidget, deleteWidget}) => {
+    const [editingStatus, setEditingStatus] = useState(false);
+    const [cachedWidget, setCachedWidget] = useState(widget);
+    return(
+        <>
+            {
+                editingStatus &&
+                <>
+                    <i onClick={() => {
+                        updateWidget(cachedWidget)
+                        setEditingStatus(false);
+                    }} className="fas fa-check float-right"></i>
+                    <i onClick={() => {
+                        deleteWidget(widget.id);
+                        setEditingStatus(false);
+                    }} className="fas fa-trash float-right"></i>
+                    <input value={widget.text} className="form-control"/>
+                    <select value={widget.size} className="form-control">
+                        <option value={1}>Heading 1</option>
+                        <option value={2}>Heading 2</option>
+                        <option value={3}>Heading 3</option>
+                        <option value={4}>Heading 4</option>
+                        <option value={5}>Heading 5</option>
+                        <option value={6}>Heading 6</option>
+                    </select>
+                    <select value={cachedWidget.type} className="form-control"
+                            onChange = {(event) => setCachedWidget({
+                                ...widget, type: event.target.value
+                            })}>
+                        <option value='HEADING'>Heading</option>
+                        <option value='PARAGRAPH'>Paragraph</option>
+                        <option value='VIDEO'>Video</option>
+                        <option value='IMAGE'>Image</option>
+                        <option value='LINK'>Link</option>
+                        <option value='LIST'>List</option>
+                        <option value='HTML'>HTML</option>
+                    </select>
+                </>
+            }
+            {
+                !editingStatus &&
+                <>
+                    <i onClick={() => setEditingStatus(true)} className="fas fa-2x fa-cog float-right"></i>
+                    {widget.size === 1 && <h1>{widget.text}</h1>}
+                    {widget.size === 2 && <h2>{widget.text}</h2>}
+                    {widget.size === 3 && <h3>{widget.text}</h3>}
+                    {widget.size === 4 && <h4>{widget.text}</h4>}
+                    {widget.size === 5 && <h5>{widget.text}</h5>}
+                    {widget.size === 6 && <h6>{widget.text}</h6>}
+                </>
+            }
+        </>
+    )
 }
 
 export default HeadingWidget
--- src/components/course-editor/widgets/paragraph-widget.js
+++ src/components/course-editor/widgets/paragraph-widget.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const ParagraphWidget = () => {
-
+const ParagraphWidget = ({widget, updateWidget, deleteWidget}) => {
+    const [editingStatus, setEditingStatus] = useState(false);
+    const [cachedWidget, setCachedWidget] = useState(widget);
+    return(
+        <>
+            {
+                editingStatus &&
+                <>
+                    <i onClick={() => {
+                        updateWidget(cachedWidget)
+                        setEditingStatus(false);
+                    }} className="fas fa-check float-right"></i>
+                    <i onClick={() => {
+                        deleteWidget(widget.id);
+                        setEditingStatus(false);
+                    }} className="fas fa-trash float-right"></i>
+                    <select value={cachedWidget.type} className="form-control"
+                            onChange = {(event) => setCachedWidget({
+                                ...widget, type: event.target.value
+                            })}>
+                        <option value='HEADING'>Heading</option>
+                        <option value='PARAGRAPH'>Paragraph</option>
+                        <option value='VIDEO'>Video</option>
+                        <option value='IMAGE'>Image</option>
+                        <option value='LINK'>Link</option>
+                        <option value='LIST'>List</option>
+                        <option value='HTML'>HTML</option>
+                    </select>
+                    <textarea value={cachedWidget.text} className="form-control" id = 'ta1' onChange={
+                        (event) => {
+                            let value = document.getElementById("ta1");
+                            console.log('cachedWidgetVVV', value);
+                            setCachedWidget({
+                                ...widget, text: event.target.value
+                            })
+                        }
+                    }
+                    ></textarea>
+                </>
+            }
+            {
+                !editingStatus &&
+                <>
+                    <i onClick={() => setEditingStatus(true)} className="fas fa-2x fa-cog float-right"></i>
+                    <p>
+                        {widget.text}
+                    </p>
+                </>
+            }
+        </>
+    )
 }
 
 export default ParagraphWidget
