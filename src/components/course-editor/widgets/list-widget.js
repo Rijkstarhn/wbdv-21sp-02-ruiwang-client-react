66755761e3b46b358d27d7ff14959ea3dac92d67
Rijkstarhn <32129291+Rijkstarhn@users.noreply.github.com>
2021-03-25 23:31:54 -0700

    Finished List Widget Component.

--- src/components/course-editor/widgets/list-widget.js
+++ src/components/course-editor/widgets/list-widget.js
@@ -1,31 +1,74 @@
 import React, {useState} from 'react';
 
-const ListWidget = () => {
+const ListWidget = ({updateWidget, deleteWidget, widget}) => {
 
-    const [editing, setEditing] = useState(false);
+    const [editingStatus, setEditingStatus] = useState(false);
+    const [cachedWidget, setCachedWidget] = useState(widget);
 
     return (
         <div>
             {
-                editing &&
+                editingStatus &&
                     <>
-                        <input type = "checkbox"/> Ordered
+                        <input
+                            onChange = {(event) => {
+                            // here should be 'isOrderedList: !cachedWidget.isOrderedList' because
+                            // the onChange event handler happens after onChange happened.
+                            setCachedWidget({
+                                ...cachedWidget, orderedList: !cachedWidget.orderedList
+                            })
+                        }}
+                            checked = {cachedWidget.orderedList}
+                            type = "checkbox"/> Ordered
+                        <i onClick={() => {
+                            console.log(cachedWidget)
+                            updateWidget(cachedWidget)
+                            setEditingStatus(false);
+                        }} className="fas fa-check float-right"></i>
+                        <i onClick={() => {
+                            deleteWidget(widget.id);
+                            setEditingStatus(false);
+                        }} className="fas fa-trash float-right"></i>
                         <br/>
                         Item list
-                        <textarea className='form-control'>
+                        <textarea onChange = {(event) => setCachedWidget(
+                            {...cachedWidget, text: event.target.value}
+                        )}
+                                  rows={10}
+                                  value = {cachedWidget.text}
+                                  className='form-control'>
 
                         </textarea>
                     </>
             }
             {
-                !editing &&
+                !editingStatus &&
                     <>
-                        <ul>
-                            <li>123</li>
-                            <li>234</li>
-                            <li>321</li>
-                            <li>432</li>
-                        </ul>
+                        <i onClick={() => setEditingStatus(true)} className="fas fa-2x fa-cog float-right"></i>
+                        {
+                            !cachedWidget.orderedList &&
+                            <>
+                                <ul>
+                                    {
+                                        cachedWidget.text.split('\n').map(item => <li>
+                                            {item}
+                                        </li>)
+                                    }
+                                </ul>
+                            </>
+                        }
+                        {
+                            cachedWidget.orderedList &&
+                            <>
+                                <ol>
+                                    {
+                                        cachedWidget.text.split('\n').map(item => <li>
+                                            {item}
+                                        </li>)
+                                    }
+                                </ol>
+                            </>
+                        }
                     </>
             }
         </div>
