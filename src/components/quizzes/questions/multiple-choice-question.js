62729f245ef6f3e286611068a8eef4dbf53c60ce
Rijkstarhn <32129291+Rijkstarhn@users.noreply.github.com>
2021-04-04 12:43:12 -0700

    Created question and two different type of question modules.

---
+++ src/components/quizzes/questions/multiple-choice-question.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+
+const MultipleChoiceQuestion = ({question}) => {
+
+    const [userAnswer, setUserAnswer] = useState('')
+    const [isCorrect, setIsCorrect] = useState(null)
+
+    return (
+        <div>
+            <h3>{question.question}
+                {isCorrect && <i className="fas fa-check checkIconColor"></i>}
+                {isCorrect === false && <i className="fas fa-times crossIconColor"></i>}</h3>
+            <ul className = 'list-group no-bullets'>
+                {
+                    question.choices.map((choice, index) =>
+                        <li key = {index}>
+                            <label
+                                className = {`list-group-item 
+                                    ${isCorrect !== null && question.correct === choice && 'list-group-item-success'}
+                                    ${isCorrect !== null && userAnswer === choice && userAnswer !== question.correct && 'list-group-item-danger'}`} >
+                                <input type='radio' onClick={() => setUserAnswer(choice)} name={question._id}/> {choice}
+                                {isCorrect !== null && question.correct === choice && <i className="fas fa-check checkIconColor float-right"></i>}
+                                {isCorrect !== null && userAnswer !== question.correct && userAnswer === choice && <i className="fas fa-times crossIconColor float-right"></i>}
+                            </label>
+                        </li>)
+                }
+            </ul>
+            <h6>Your Answer: {userAnswer}</h6>
+            <button onClick = {() => {
+                if (userAnswer === question.correct) {
+                    setIsCorrect(true)
+                } else {
+                    setIsCorrect(false)
+                }
+            }}
+                    className = 'btn btn-primary'>
+                Grade
+            </button>
+        </div>
+    )
+}
+
+export default MultipleChoiceQuestion;
---
+++ src/components/quizzes/questions/true-false-question.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react'
+
+const TrueFalseQuestion = ({question}) => {
+
+    const [userAnswer, setUserAnswer] = useState('')
+    const [isCorrect, setIsCorrect] = useState(null)
+
+    return (
+        <div>
+            <h3>{question.question}
+            {isCorrect && <i className="fas fa-check checkIconColor"></i>}
+            {isCorrect === false && <i className="fas fa-times crossIconColor"></i>}</h3>
+            <ul className = 'list-group no-bullets'>
+                <li>
+                    <label
+                        className = {`list-group-item 
+                            ${isCorrect !== null && question.correct === 'true' && 'list-group-item-success'}
+                            ${isCorrect !== null && userAnswer === 'true' && userAnswer !== question.correct && 'list-group-item-danger'}`} >
+                        <input type='radio' onClick={() => setUserAnswer('true')} name={question._id}/> TRUE
+                        {isCorrect !== null && question.correct === 'true' && <i className="fas fa-check checkIconColor float-right"></i>}
+                        {isCorrect !== null && userAnswer !== question.correct && userAnswer === 'true' && <i className="fas fa-times crossIconColor float-right"></i>}
+                    </label>
+                </li>
+                <li>
+                    <label
+                        className = {`list-group-item 
+                            ${isCorrect !== null && question.correct === 'false' && 'list-group-item-success'}
+                            ${isCorrect !== null && userAnswer === 'false' && userAnswer !== question.correct && 'list-group-item-danger'}`} >
+                        <input type='radio' onClick={() => setUserAnswer('false')} name={question._id}/> FALSE
+                        {isCorrect !== null && question.correct === 'false' && <i className="fas fa-check checkIconColor float-right"></i>}
+                        {isCorrect !== null && userAnswer !== question.correct && userAnswer === 'false' && <i className="fas fa-times crossIconColor float-right"></i>}
+                    </label>
+                </li>
+            </ul>
+            <h6>Your Answer: {userAnswer}</h6>
+            <button onClick = {() => {
+                if (userAnswer === question.correct) {
+                    setIsCorrect(true)
+                } else {
+                    setIsCorrect(false)
+                }
+            }}
+                    className = 'btn btn-primary'>
+                Grade
+            </button>
+        </div>
+    )
+}
+
+export default TrueFalseQuestion;
